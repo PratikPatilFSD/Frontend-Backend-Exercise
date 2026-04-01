39d264faa57f2908909773612c847261fcb23993
PratikPatilFSD <pripat2610@gmail.com>
2026-04-01 23:41:01 +0530

    Add files via upload

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+// ================= IMPORTS =================
+const { Pool } = require("pg");
+
+// ================= DATABASE CONNECTION =================
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+
+  ssl: process.env.NODE_ENV === "production"
+    ? { rejectUnauthorized: false }
+    : false,
+});
+
+// ================= EXPORT =================
+module.exports = pool;
